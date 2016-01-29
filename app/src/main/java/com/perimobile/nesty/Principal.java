5fdded77a7829d14af6942b85324fc870e26f4ed
otavioafpedro <otavioafpedro@hotmail.com>
2016-01-28 23:43:48 -0200

    Login Google+
    O Login está feito, só falta fazer o POST...
    O Login do Facebook eu deixei para amanhã a tarde pois preciso decidir
    uma coisa antes sobre como será e sobre fragments se não irei fazer
    trabalho dobrado.

--- app/src/main/java/com/perimobile/nesty/Principal.java
+++ app/src/main/java/com/perimobile/nesty/Principal.java
@@ -1,23 +1,223 @@
 package com.perimobile.nesty;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.perimobile.nesty.Entidades.Cliente;
+import com.squareup.picasso.Picasso;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     // List<Imovel> imoveis;
     // ImovelAdapter adapter;
     // ListView listView;
     final static String IDIMOV = "id";
     final static String IDIMOB = "id_imob";
+
+    private static final int RC_SIGN_IN = 0;
+    private static final String TAG = Principal.class.getSimpleName();
+
+    private SignInButton btnGPlus;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mIntentInProgress;
+    private boolean mSignInClicked;
+    private ConnectionResult mConnectionResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+
+
+        btnGPlus = (SignInButton) findViewById(R.id.sign_in_button);
+
+        btnGPlus.setOnClickListener(onLoginListener());
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
     }
+
+
+    private View.OnClickListener onLoginListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.sign_in_button && !mGoogleApiClient.isConnected() && !mGoogleApiClient.isConnecting()) {
+                    mSignInClicked = true;
+                    login();
+                }
+                //signInGooglePlusAccount();
+            }
+        };
+    }
+
+    protected void onActivityResult (int requestCode, int responseCode, Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            if (responseCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mSignInClicked = false;
+        Toast.makeText(this, R.string.conexaosucesso, Toast.LENGTH_LONG).show();
+
+        getUserInformation();
+
+        layoutAction(true);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+        layoutAction(false);
+    }
+
+    private void layoutAction(boolean isSignedIn) {
+        if (isSignedIn) {
+            //levar para mapa ou lista.
+        } else {
+            //manter na página de login.
+        }
+    }
+
+    private void signInGooglePlusAccount() {
+        if (!mGoogleApiClient.isConnecting()) {
+            mSignInClicked = true;
+            resolveSignInError();
+        }
+    }
+
+    private void resolveSignInError() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+            } catch (IntentSender.SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!result.hasResolution()) {
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
+                    0).show();
+            return;
+        }
+
+        if (!mIntentInProgress) {
+            mConnectionResult = result;
+
+            if (mSignInClicked) {
+                resolveSignInError();
+            }
+        }
+
+    }
+
+    public void login() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+            } catch (IntentSender.SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    private void getUserInformation() {
+        try {
+            if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+                Person person = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+                String clienteDisplayName = person.getDisplayName(); // user account name
+                String clienteImageUrl = person.getImage().getUrl(); // profile image url
+                String clienteemail = Plus.AccountApi.getAccountName(mGoogleApiClient); // account Email
+
+                //Fazer POST!!!
+
+                /*                
+                           //transforma a URL em imagem, no esquema de deixar uma imagem padrão.
+                            Picasso.with(this)
+                                    .load(avatarUrl)
+                                    .placeholder(R.mipmap.ic_launcher)
+                                    .error(R.mipmap.ic_launcher)
+                                    .into(avatar); */
+
+                } else {
+                Log.e(TAG, "profile is null");
+                }
+            } catch (Exception ex) {
+            ex.printStackTrace();
+            }
+    }
+
+    /*@Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.splash, container, false);
+
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        // If using in a fragment
+        loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,6 +239,19 @@ public class Principal extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onStart(){
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop(){
+        super.onStop();
+
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     @Override
