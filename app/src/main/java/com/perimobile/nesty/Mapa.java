c7a928251ed10b36f57d1c0bf4e8f6eb70d6622e
otavioafpedro <otavioafpedro@hotmail.com>
2016-02-08 22:24:37 -0200

    Mudança infowindow do GoogleMap
    É nois...

--- app/src/main/java/com/perimobile/nesty/Mapa.java
+++ app/src/main/java/com/perimobile/nesty/Mapa.java
@@ -1,16 +1,28 @@
 package com.perimobile.nesty;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,18 +31,27 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.perimobile.nesty.Entidades.Imovel;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class Mapa extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
+public class Mapa extends AppCompatActivity implements LocationListener, OnMapReadyCallback, OnInfoWindowClickListener {
 
     LatLng mOrigem;
     private ImovelTask mTask;
     private List<Imovel> imoveis;
     GoogleMap mGoogleMap;
     HttpJson imovelHTTPmapa;
+    RadioGroup mOptions;
+    private Marker mLastSelectedMarker;
+    private Long id;
+    private Context ctx;
+    private ImageLoader mLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +65,10 @@ public class Mapa extends AppCompatActivity implements LocationListener, OnMapRe
             }
         });
 
-        imovelHTTPmapa = new HttpJson(Principal.URLBASE+"?opt=prev","");
+        this.ctx = this;
+        mLoader = PatternVolley.getInstance(ctx).getImageLoader();
+
+        imovelHTTPmapa = new HttpJson(Principal.URLBASE + "?opt=prev", "");
         imovelHTTPmapa.leitura = new LerImovelResumido();
 
         int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
@@ -59,31 +83,18 @@ public class Mapa extends AppCompatActivity implements LocationListener, OnMapRe
 
             // Pegando referencia do SupportMapFragment para a acitvity_map.xml
             SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-            // Pegando o Objeto GoogleMaps do fragment.
-            //mGoogleMap = fm.getMap();
             fm.getMapAsync(this);
 
-
-            // Deixando o mapa do tipo satelite.
-            /*mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            // Obtendo o Objeto LocationManager do LOCATION_SERVICE.
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            // Criando um objetico de criterio para voltar para o provedor.
-            Criteria criteria = new Criteria();
-            // Conseguindo o nome do melhor provedor.
-            String provider = locationManager.getBestProvider(criteria, true);
-            // Obtendo a localização atual.
-            Location location = locationManager.getLastKnownLocation(provider);
-            // Permissões.
-            if (location != null) {
-                onLocationChanged(location);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    return;
+            /*mOptions = (RadioGroup) findViewById(R.id.custom_info_window_options);
+            mOptions.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    if (mLastSelectedMarker != null && mLastSelectedMarker.isInfoWindowShown()) {
+                        // Refresh the info window when the info window's content has changed.
+                        mLastSelectedMarker.showInfoWindow();
+                    }
                 }
-            }
-            locationManager.requestLocationUpdates(provider, 20000, 0, this);*/
+            });*/
         }
     }
 
@@ -108,15 +119,29 @@ public class Mapa extends AppCompatActivity implements LocationListener, OnMapRe
             for (int i = 0; i < imoveis.size(); i++) {
 
                 String preco = String.format(res.getString(R.string.preco), imoveis.get(i).getPreco());
+                id = imoveis.get(i).getId();
                 mGoogleMap.addMarker(new MarkerOptions()
                         .position(new LatLng(imoveis.get(i).getLat(), imoveis.get(i).getLng()))
                         .title(String.valueOf(preco))
+                        .snippet(imoveis.get(i).getEndereco())
                         .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
             }
 
             // Possibilitando a minha localização no GoogleMap.
-            mGoogleMap.setMyLocationEnabled(true);
+            /*if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }*/
+            //mGoogleMap.setMyLocationEnabled(true);
+
+            // Set listeners for marker events.  See the bottom of this class for their behavior.
+
         }
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        // Colocar o contexto e fazer a intent do imovel detalhe começar.
+        //Toast.makeText(this, "Click Info Window", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -151,6 +176,11 @@ public class Mapa extends AppCompatActivity implements LocationListener, OnMapRe
     public void onMapReady(final GoogleMap googleMap) {
         mGoogleMap = googleMap;
 
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(false);
+        mGoogleMap.setOnInfoWindowClickListener(this);
+        mGoogleMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+
+
         if (mTask == null) {
             mTask = new ImovelTask();
             mTask.execute();
@@ -164,4 +194,78 @@ public class Mapa extends AppCompatActivity implements LocationListener, OnMapRe
         }
 
     }
+
+    class CustomInfoWindowAdapter implements InfoWindowAdapter {
+
+        private final View mWindow;
+
+        private final View mContents;
+
+        CustomInfoWindowAdapter() {
+            mWindow = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+            mContents = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            /*if (mOptions.getCheckedRadioButtonId() != R.id.custom_info_window) {
+                // This means that getInfoContents will be called.
+                return null;
+            }*/
+            render(marker, mWindow);
+            return mWindow;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            /*if (mOptions.getCheckedRadioButtonId() != R.id.custom_info_contents) {
+                // This means that the default info contents will be used.
+                return null;
+            }*/
+            render(marker, mContents);
+            return mContents;
+        }
+
+        private void render(Marker marker, View view) {
+            String URLImg = "http://www.perimobile.com/img";
+            int azulnesty = getResources().getColor(R.color.azulnesty);
+            int vermelhonesty = getResources().getColor(R.color.vescuronesty);
+            /*for (int i = 0; i < imoveis.size(); i++) {
+
+                if (imoveis.get(i).getId() == id) {
+                    NetworkImageView badge = (NetworkImageView) findViewById(R.id.badge);
+                    badge.setImageUrl(URLImg + imoveis.get(i).getImgPrincipal(), mLoader);
+                    ImageView badge = (ImageView) findViewById(R.id.badge);
+
+                    Picasso.with(ctx)
+                            .load(URLImg + imoveis.get(i).getImgPrincipal())
+                            .into(badge);
+                } else {
+                    int badge = R.mipmap.ic_launcher;
+                    ((NetworkImageView) view.findViewById(R.id.badge)).setImageResource(badge);
+                }*/
+
+                String title = marker.getTitle();
+                TextView titleUi = ((TextView) view.findViewById(R.id.title));
+                if (title != null) {
+                    // Spannable string allows us to edit the formatting of the text.
+                    SpannableString titleText = new SpannableString(title);
+                    titleText.setSpan(new ForegroundColorSpan(azulnesty), 0, titleText.length(), 0);
+                    titleUi.setText(titleText);
+                } else {
+                    titleUi.setText("");
+                }
+
+                String snippet = marker.getSnippet();
+                TextView snippetUi = ((TextView) view.findViewById(R.id.snippet));
+                if (snippet != null && snippet.length() > 12) {
+                    SpannableString snippetText = new SpannableString(snippet);
+                    snippetText.setSpan(new ForegroundColorSpan(vermelhonesty), 0, snippet.length(), 0);
+                    snippetUi.setText(snippetText);
+                } else {
+                    snippetUi.setText("");}
+        }
+
+    }
 }
+
